56579c87f2c79d023c3711d075c165be983604d0
Reeyyyh <rhnlnv@gmail.com>
2023-06-21 21:42:22 +0700

    update with array

--- Main.lua
+++ Main.lua
@@ -1,32 +1,32 @@
 -- sistem kasir lua try
-local harga1, harga2, harga3 = 15000, 13000, 11000
-local menu1, menu2, menu3 = "kopi original", "kopi Mocha", "Bebek cappucino"
+local harga = {15000, 13000, 11000}
+local menu = {"kopi original", "kopi Mocha", "Kopi cappucino"}
 local total = 0
 
 print("=========== daftar menu ==========")
-print(string.format("1.%s \t| Rp.%i", menu1, harga1))
-print(string.format("2.%s \t\t| Rp.%i", menu2, harga2))
-print(string.format("3.%s \t| Rp.%i", menu3, harga3))
+print(string.format("1.%s \t| Rp.%i", menu[1], harga[1]))
+print(string.format("2.%s \t\t| Rp.%i", menu[2], harga[2]))
+print(string.format("3.%s \t| Rp.%i", menu[3], harga[3]))
 --print("0.check out")
 io.write("pilih menu : ")
 
 local pilih = tonumber(io.read())
 
 if (pilih == 1) then
-    print(string.format("%s | Rp.%i", menu1, harga1))
+    print(string.format("%s | Rp.%i", menu[1], harga[1]))
     io.write("Masukan jumlah pesanan : ")
     local pesan = tonumber(io.read())
-    total = harga1 * pesan
+    total = harga[1] * pesan
 elseif (pilih == 2) then
-    print(string.format("%s | Rp.%i", menu2, harga2))
+    print(string.format("%s | Rp.%i", menu[2], harga[2]))
     io.write("Masukan jumlah pesanan : ")
     local pesan = tonumber(io.read())
-    total = harga2 * pesan
+    total = harga[2] * pesan
 elseif (pilih == 3) then
-    print(string.format("%s | Rp.%i", menu3, harga3))
+    print(string.format("%s | Rp.%i", menu[3], harga[3]))
     io.write("Masukan jumlah pesanan : ")
     local pesan = tonumber(io.read())
-    total = harga3 * pesan
+    total = harga[3] * pesan
 else
     print("menu tidak tersedia")
 end
@@ -34,5 +34,5 @@ end
 print("Total Harga : "..total)
 
 --[[
-    still need a loops, array
+    still need a loops
 ]]
